737e6291d920f9bba533c25dfa6485660cecd5e0
Eun Woo Son <eunwoo.son@sdstate.edu>
2018-02-26 18:46:08 -0600

    host

--- pages/thw.vue
+++ pages/thw.vue
@@ -224,7 +224,7 @@ export default {
       containers: [],
       msg_docker_result: '',
       //host: 'http://bioinformatics.sdstate.edu:8000',
-      host: '0.0.0.0:3001',
+      host: 'galnb.com:3001',
     }
   },
   mounted() {
